aa312fc6e59b6bc65cdcf557dbdf99e5e43e83d6
BahmaJewelry <eijewelry20@gmail.com>
2026-06-02 22:14:50 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'bahma-v1';
+const ASSETS = ['/', '/index.html', '/icon.png', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        if(res && res.status === 200 && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
